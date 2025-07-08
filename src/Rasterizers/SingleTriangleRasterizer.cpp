9980520d6fad0f965b4b59e17d0bcfad8bf31031
Elias Aggergaard Larsen <s235125@dtu.dk>
2025-07-08 20:55:49 +0200

    Added cube model and now draws 250 random triangles

--- src/Rasterizers/SingleTriangleRasterizer.cpp
+++ src/Rasterizers/SingleTriangleRasterizer.cpp
@@ -3,6 +3,11 @@
 //
 
 #include "SingleTriangleRasterizer.h"
+
+#include <iostream>
+#include <ostream>
+#include <random>
+
 #include "../EnvVariables.h"
 #include "../MathUtil.h"
 #include "../models/Double2.h"
@@ -11,21 +16,42 @@
 
 std::unique_ptr<ImageInterface> SingleTriangleRasterizer::rasterize()
 {
-  Double2 p1 = Double2(500, 700);
-  Double2 p2 = Double2(500, 200);
-  Double2 p3 = Double2(1000, 200);
+  std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
+
+  std::random_device dev;
+  std::mt19937 rng(dev());
+  std::uniform_int_distribution<std::mt19937::result_type> randX(0, 1280); // distribution in range [1, 6]
+  std::uniform_int_distribution<std::mt19937::result_type> randY(0, 720); // distribution in range [1, 6]
+  std::uniform_int_distribution<std::mt19937::result_type> randColor(0, 255); // distribution in range [1, 6]
 
   std::vector<std::vector<Pixel>> pixels(EnvVariables::screenWidth);
-  for (int i = 0; i < EnvVariables::screenWidth; i++)
+  for (int _ = 0; _ < 250; _++)
   {
-    pixels[i].resize(EnvVariables::screenHeight);
-    for (int j = 0; j < EnvVariables::screenHeight; j++)
+    Double2 p1 = Double2(randX(rng), randY(rng));
+    Double2 p2 = Double2(randX(rng), randY(rng));
+    Double2 p3 = Double2(randX(rng), randY(rng));
+
+    int red = randColor(rng);
+    int green = randColor(rng);
+    int blue = randColor(rng);
+
+
+    for (int i = 0; i < EnvVariables::screenWidth; i++)
     {
-      if (MathUtil::PointInsideTriangle(p1, p2, p3, {static_cast<double>(i), static_cast<double>(j)}))
+      pixels[i].resize(EnvVariables::screenHeight);
+
+
+      for (int j = 0; j < EnvVariables::screenHeight; j++)
       {
-        pixels[i][j] = Pixel(255, 0, 0);
+        if (MathUtil::PointInsideTriangle(p1, p2, p3, {static_cast<double>(i), static_cast<double>(j)}))
+        {
+          pixels[i][j] = Pixel(red, green, blue);
+        }
       }
     }
   }
+  std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+  std::cout << "Pixel calculation took: " << duration.count() << "ms" << std::endl;
   return std::make_unique<ImageSlow>(pixels);
 }
