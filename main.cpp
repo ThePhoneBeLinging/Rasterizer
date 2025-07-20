a220f7a44e618871909f5ae46ced6c992d6ab147
Elias Aggergaard Larsen <s235125@dtu.dk>
2025-07-20 14:52:46 +0200

    feat: added 3 cubes

--- main.cpp
+++ main.cpp
@@ -12,11 +12,11 @@ int main()
   InitWindow(EnvVariables::screenWidth, EnvVariables::screenHeight, "Rasterizer");
   std::vector<RasModel*> models;
   auto cube = ModelLoader::getRasModel("simpleCube");
-  for (int i = 0; i < 1; i++)
+  for (int i = -1; i < 2; i++)
   {
     auto cubeInstance = cube->createInstance();
     cubeInstance->scale_ = 100;
-    cubeInstance->position_.x = 0;
+    cubeInstance->position_.x = -200 * i;
     cubeInstance->position_.y = 0;
     cube->generateTriangles();
     models.push_back(cube);
