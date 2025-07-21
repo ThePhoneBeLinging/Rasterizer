67ae08bdea6c5eba52e0f5843c819b0a0449ade4
EAL <elias@takefive.dk>
2025-07-21 23:05:21 +0200

    fix: Cube is now longer duplicated on top of itself

--- main.cpp
+++ main.cpp
@@ -18,9 +18,9 @@ int main()
     cubeInstance->scale_ = 100;
     cubeInstance->position_.x = -200 * i;
     cubeInstance->position_.y = 0;
-    cube->generateTriangles();
-    models.push_back(cube);
   }
+  cube->generateTriangles();
+  models.push_back(cube);
   ImageSlow image = ImageSlow();
   for (int times = 0; times < 2500; times++)
   {
